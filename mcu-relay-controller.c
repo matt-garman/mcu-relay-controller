9fa148100a39825cc90f44d8c22f814e027375da
matt-garman <matthew.garman@gmail.com>
2023-05-05 17:12:25 -0500

    add ability to build for ATtiny

--- mcu-relay-controller.c
+++ mcu-relay-controller.c
@@ -21,6 +21,16 @@
 #define OFF 0
 #define ON 1
 
+
+// include hardware-specific implementation file(s)
+#ifdef IMPL_DUMMY
+#  include "dummy.h"
+#endif
+#ifdef IMPL_ATTINY
+#  include "attiny.h"
+#endif
+
+
 // the relay has two states, which we'll call ON or OFF
 volatile uint8_t relay_state = OFF;
 
@@ -50,14 +60,20 @@ void relay_toggle()
 
 int main(int argc, char* argv[])
 {
+    // initialize hardware
     MRC_hardware_init();
+
+    // make relay and status indicator LED consistent consistent with the
+    // default relay_state = OFF
+    relay_deactivate();
+    MRC_led_pin_set_low();
 
     while (1)
     {
         MRC_disable_interrupts();
         MRC_disable_sleep();
 
-        if (0 == MRC_get_switch_pin_state())
+        if (0 == MRC_switch_pin_get_state())
         {
             relay_toggle();
             MRC_led_toggle();
