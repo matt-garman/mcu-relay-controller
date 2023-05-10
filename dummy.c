608763be97bcfc1b618fb4465b8b93212ee9a7a7
matt-garman <matthew.garman@gmail.com>
2023-05-10 11:59:48 -0500

    function sigs to match header

--- dummy.c
+++ dummy.c
@@ -10,20 +10,22 @@
  *    gcc -Wall -ggdb3 -Os mcu-relay-controller.c dummy.c
  */
 
+#include "dummy.h"
+
 #include "mcu-relay-controller-iface.h"
 
-
-void MRC_hardware_init() { }
-void MRC_disable_interrupts() { }
-void MRC_disable_sleep() { }
-void MRC_enable_interrupts() { }
-void MRC_enter_sleep_mode() { }
-void MRC_led_pin_set_high() { }
-void MRC_led_pin_set_low() { }
-void MRC_led_toggle() { }
-void MRC_relay_coil_pin1_set_high() { }
-void MRC_relay_coil_pin1_set_low() { }
-void MRC_relay_coil_pin2_set_high() { }
-void MRC_relay_coil_pin2_set_low() { }
-uint8_t MRC_switch_pin_get_state() { return 0; }
+void MRC_hardware_init(void) { }
+void MRC_disable_interrupts(void) { }
+void MRC_disable_sleep(void) { }
+void MRC_enable_interrupts(void) { }
+void MRC_enter_sleep_mode(void) { }
+void MRC_led_pin_set_high(void) { }
+void MRC_led_pin_set_low(void) { }
+void MRC_led_toggle(void) { }
+void MRC_relay_coil_pin1_set_high(void) { }
+void MRC_relay_coil_pin1_set_low(void) { }
+void MRC_relay_coil_pin2_set_high(void) { }
+void MRC_relay_coil_pin2_set_low(void) { }
+uint8_t MRC_switch_pin_get_state(void) { return 0; }
+void MRC_switch_pin_clear_int_flags(void) { }
 
