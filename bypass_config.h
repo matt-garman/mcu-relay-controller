c621a450d1fcea8854579ccb5d3251b630525abd
matt-garman <matthew.garman@gmail.com>
2026-06-12 09:38:50 -0500

    break conditional hardware config + global constants out into config header

---
+++ bypass_config.h
@@ -0,0 +1,67 @@
+#ifndef BYPASS_CONFIG_H__
+#define BYPASS_CONFIG_H__
+
+
+// - core design assumes a 1ms timer interrupt (within the bounds of the MCU's
+//   built-in RC oscillator precision)
+// - timing is therefore dependent on MCU operating speed; timer math based on
+//   the CPU clock rate (1.2MHz for ATtiny13/a, 1.0MHz for ATtiny85)
+#if defined(__AVR_ATtiny85__)
+#  if !defined(F_CPU)
+#    error "F_CPU must be defined via build flags, e.g. -DF_CPU=1000000UL"
+#  endif
+#  if (F_CPU != 1000000UL)
+#    error "F_CPU must be 1000000 for ATtiny85 (CKDIV8 enabled, 8MHz internal RC)"
+#  endif
+// ATtiny85 uses TIMSK, not TIMSK0; alias for shared code (in C file)
+#  define TIMSK0 TIMSK
+#else
+#  if !defined(F_CPU)
+#    error "F_CPU must be defined via build flags, e.g. -DF_CPU=1200000UL"
+#  endif
+#  if (F_CPU != 1200000UL)
+#    error "F_CPU must be 1200000 for ATtiny13/a"
+#  endif
+#endif
+
+
+// used named constants for GPIO pin functions for readability
+#define FOOTSW_PIN (PB0)
+#define LED_PIN    (PB1)
+#define CD4053_PIN (PB2)
+
+
+// number of HIGH PB0/footswitch pin reads to be considered 
+// release-debounced, i.e. the "lock-out" period
+#define RELEASE_THRESH (25)
+
+// number of LOW PB0/footswitch pin reads to be considered
+// press-debounced
+//
+// trying to balance between "responds immediately" and "immune to
+// spurious interrupts": we can't readily distinguish between
+// environmental noise (that we want to filter/ignore as entirely)
+// versus noise from an old/bouncy switch.
+//
+// note also that PRESSED_THRESH is the *minimum* time to confirmed
+// press-debounce: a noisy switch or environmental EMI/RFI could
+// increase the time to confirmed press-debounce
+//
+// the asymmetry between PRESSED_THRESH and RELEASE_THRESH is
+// to bias the debounce time after the actual action (effect
+// engage/bypass) to balance responsiveness with robust switch
+// de-bouncing
+#define PRESSED_THRESH (8)
+
+// Timer0 CTC config for a 1ms tick:
+//   ATtiny13/a (F_CPU=1.2MHz): 8 * (149 + 1) / 1200000  -> exactly 1ms
+//   ATtiny85   (F_CPU=1.0MHz): 8 * (124 + 1) / 1000000  -> exactly 1ms
+#if defined(__AVR_ATtiny85__)
+#  define TIMER0_OCR0A_1MS (124)
+#else // ATtiny13a
+#  define TIMER0_OCR0A_1MS (149)
+#endif
+ 
+
+
+#endif // BYPASS_CONFIG_H__
